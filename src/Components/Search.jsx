21a2dfd52aa3c3ab084ecca3cc32064c1aa754d1
SRomadiah <diyah1888@gmail.com>
2022-10-31 11:56:35 +0800

    Day 5

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -7,11 +7,11 @@ const Search = () => {
   const [err, setErr] = useState(false);
 
   const handleSearch = async () => {
-    const q = query(collection(db, "users"), where("displayName", "===", username));
+    const q = query(collection(db, "users"), where("displayName", "==", username));
     try {
       const querySnapshot = await getDocs(q);
       querySnapshot.forEach((doc) => {
-        setUser(doc.data);
+        setUser(doc.data());
       });
     } catch (err) {
       setErr(true);
